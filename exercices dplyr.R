ed0fbf1ad5c3f82c5adee61995c92cdc11f6a902
EricMenetre <33350889+ERPeric@users.noreply.github.com>
2019-11-05 00:07:38 +0100

    Création de la correction des exercices

--- exercices dplyr.R
+++ exercices dplyr.R
@@ -5,7 +5,14 @@
 # Ci-après vous trouverez les exercices en lien avec le séminaire
 # EEG 
 
+library(readxl)
+data_exercices <- read_excel("data_exercices.xlsx")
+View(data_exercices)
+
+
 ##----------------------------------------------------------------
 ##                          Exercice 1                           -
 ##----------------------------------------------------------------
+
+
 
